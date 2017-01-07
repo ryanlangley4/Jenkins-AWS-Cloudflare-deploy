9ae5dc50ba75bb8b38d7cfdc6461ea8b81db2ecc
ryanlangley4 <ryan.langley4@gmail.com>
2017-01-07 11:24:02 -0800

    jenkins builds files
    2 builds for jenkins auto deploy.

---
+++ Build1_aws_deploy.ps1
@@ -0,0 +1,103 @@
+import-module awspowershell
+cd $env:WORKSPACE
+
+
+Set-AWSCredentials -ProfileName $aws_profile
+
+$aws_profile = $ENV:AWS_Profile
+$region = $ENV:Region
+$instance_name = $ENV:Instance_Name
+$builder = $ENV:BUILD_USER
+$buildtag = $ENV:BUILD_TAG
+$image_type = $ENV:image_type
+$Instance_Type = $ENV:Instance_Type
+$domain = $ENV:Domain
+
+$SecurityGroup = $ENV:Security_Group
+try {
+$SecurityGroup_Id = Get-EC2SecurityGroup -Region "us-west-2" | where { $_.tags.value -like "$SecurityGroup" } | select -expandproperty GroupId
+echo "Security group Identification response:"
+$SecurityGroup_Id
+
+} catch {
+$_
+exit 1
+}
+
+if($instance_name.length -le 1) {
+	echo "ERROR: Instance must be named and the length must be greater than 1."
+	echo "ERROR: Instance name: $instance_name"
+	echo "ERROR: Instance name length" $instance_name.length
+	exit 1
+}
+
+
+
+try {
+$image_id = Get-EC2Image -Owner amazon, self -Region $region | where { $_.Description | select-string $image_type } | select -expandproperty ImageId
+echo "EC2 Image ID Response:"
+$image_id
+} catch {
+$_
+exit 1
+}
+
+try {
+$instance_info = New-EC2Instance -ImageId $image_id -MinCount 1 -MaxCount 1 -KeyName "master.pem" -SecurityGroupId $SecurityGroup_Id -InstanceType $instance_type -Region $region
+echo "Image generation response"
+$instance_info
+} catch {
+$_
+exit 1
+}
+
+echo "Please wait for image to fully generate"
+while($(Get-Ec2instance -instanceid $instance_info.instances.instanceid -region $region).Instances.State.Name.value -ne "running") {
+sleep 1
+}
+
+echo "Naming Instance"
+$tag = New-Object Amazon.EC2.Model.Tag
+$tag.Key = "Name"
+$tag.Value = "$instance_name"
+
+New-EC2Tag -Resource $instance_info.instances.instanceid -Tag $tag -Region $region
+echo "Tagging build information"
+$tag.Key = "BuiltBy"
+$tag.Value = "$builder"
+New-EC2Tag -Resource $instance_info.instances.instanceid -Tag $tag -Region $region
+
+$tag.Key = "BuildTag"
+$tag.Value = "$BUILDTAG"
+
+
+
+New-EC2Tag -Resource $instance_info.instances.instanceid -Tag $tag -Region $region
+
+try {
+$ellastic_ip_allocation = New-EC2Address -Region $region
+echo "Elastip IP registered:"
+$ellastic_ip_allocation
+} catch {
+echo "ERROR: Registering Ec2Address"
+$_
+exit 1
+#return $false
+}
+
+
+try {
+$response = Register-Ec2Address -instanceid $instance_info.instances.instanceid -AllocationID $ellastic_ip_allocation.allocationid -Region $region
+echo "Register EC2Address Response:"
+$response
+} catch {
+echo "ERROR: Associating EC2Address:"
+$_
+exit 1
+}
+
+$PublicIP = $ellastic_ip_allocation | select -expandproperty PublicIP
+echo "Passing Env variable $PublicIP"
+"ElasticIP = $PublicIP" | Out-file build.prop -Encoding ASCII -force
+
+exit 0
---
+++ build2_cloudflare.ps1
@@ -0,0 +1,112 @@
+$email = "<CLoud FLare email>"
+$api_key = "<Cloud Flare API key>"
+
+#Configure work environment and populate environment variables:
+cd $env:WORKSPACE
+$EIP = $env:ElasticIP
+$domain_partial = $ENV:Domain
+$domain_Instance_Name = $ENV:Instance_Name
+$domain_FQDN = $domain_Instance_Name + "." + $domain_partial
+
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Content-Type", "application/json")
+$headers.Add("X-Auth-Key", "$api_key")
+$headers.Add("X-Auth-Email", "$email")
+
+
+function get-cfzoneid() {
+Param(
+	[string] $DNSname
+)
+
+	if($result = invoke-restmethod -Uri "https://api.cloudflare.com/client/v4/zones/" -Method GET  -headers $headers | select result) {
+		if($DNSname.count -ge 1) {
+			$dns_tmp = $DNSname.split(".")
+			$zone = $dns_Tmp[$dns_tmp.count-2] + "." + $dns_Tmp[$dns_tmp.count-1]
+		}
+		
+		if ($id = $result.result | where { $_.name -match $zone} | select -expandproperty id) {
+			return $id
+		} else {
+			return $false
+		}
+	}
+}
+
+function create-CFdns() {
+Param(
+  [Parameter(Mandatory=$true)]
+  [string] $DNSname,
+  [Parameter(Mandatory=$true)]
+  [string] $type,
+  [Parameter(Mandatory=$true)]
+  [string] $ip_update,
+  [string] $id="NS"
+  )
+$uri_base = "https://api.cloudflare.com/client/v4/zones/" + $id
+
+	if(-not($result = invoke-restmethod -Uri "$uri_base/dns_records" -Method GET  -headers $headers)) {
+		return $false
+	}
+
+	if(-not ($result.result | where { $_.name -eq "$DNSname"})) {
+		try {
+			$json = "{""type"":""" + $type +""",
+					 ""name"":""" + $dnsname + """,
+					 ""content"":""" + $ip_update + """ }"
+			$result = invoke-restmethod -Uri "https://api.cloudflare.com/client/v4/zones/$id/dns_records/" -Method POST -Body $json -headers $headers
+			return $result.result
+		} catch {
+			return $false
+		}
+	} else {
+		return $false
+	}
+}
+
+function update-CFdns() {
+Param(
+  [Parameter(Mandatory=$true)]
+  [string] $DNSname,
+  [Parameter(Mandatory=$true)]
+  [string] $ip_update,
+  [string] $id="NS"
+  )
+
+$uri_base = "https://api.cloudflare.com/client/v4/zones/" + $id
+
+	if($id -eq "NS") {
+	$id = get-cfzoneid $DNSname
+	}
+
+	if(-not ($result = invoke-restmethod -Uri "$uri_base/dns_records" -Method GET  -headers $headers)) {
+	return $false
+	}
+
+	if($data = $result.result | where { $_.name -eq "$DNSname"}) {
+		try {
+			$data | add-member "content" "$ip_update" -force
+			$json = $data | ConvertTo-Json
+			$query_url = $uri_base + "/dns_records/" + $data.id
+			$result = invoke-restmethod -Uri $query_url -Method PUT -Body $json -headers $headers
+			return $result.result
+		} catch {
+			return $_
+		}
+	} else {
+		return $false
+	}
+}
+
+try {
+$id = get-cfzoneid $domain_FQDN
+if(-not(update-CFdns $domain_FQDN $EIP $id)){
+create-CFdns $domain_FQDN A $EIP $id
+}
+echo "Assigned $EIP to $domain_FQDN"
+} catch {
+echo "ERROR Assigning $EIP to $domain_FQDN"
+$_
+exit 1
+}
